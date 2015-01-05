87db664ee6a81b424ece70351f4d05775d7cd4e4
TuckerCodes <han@234.103.131.216.client.dyn.strong-sf45.reliablehosting.com>
2015-01-04 16:25:25 -0800

    Tip Calculator
    Prework for the Codepath iOS (Swift) for Designers Class

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //ways to refer to each of these items
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var percentageLabel: UILabel!
+    
+    //functions for the items
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        percentageLabel.text = "0.18"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        var tipPercentages = [0.18, 0.2, 0.22, 0.25]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+      
+        
+  var billAmount = (billField.text as NSString).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        var percentage = tipPercentage
 
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        percentageLabel.text = String(format: "%.2f", percentage)
+        
+    }
+    
+    //swipe gestures to remove keyboard
+
+    @IBAction func onTap(sender:AnyObject)
+     {
+        
+    view.endEditing(true)
+    
+    }
 }
 
